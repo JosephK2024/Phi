f5aeb915e1d971067b70c5d2dc1cbf7c2c7f6a43
Joseph Kennedy <josephstonekennedy.2024@gmail.com>
2024-01-04 21:27:22 -0500

    Shell JavaDoc additions

--- App Window/Shell.java
+++ App Window/Shell.java
@@ -1,5 +1,10 @@
 import javax.swing.*;//For JFrame access
 
+/**
+ * A JFrame extension with added Ancestry and 
+ * @author JK
+ * @version U.1
+ */
 public class Shell extends JFrame{
     //Ancestry
     Shell[] children;
@@ -9,99 +14,120 @@ public class Shell extends JFrame{
     /**
      * Constructors
      * 
-     */
+    */
 
-    /**
-     * Constructs a Shell with nothing
-     */
-    public Shell(){
-        super();
-        
-        this.children = null;
-        this.parent = null;
-    }
+        /**
+         * Constructs a {@code Shell} with 
+         * nothing
+         */
+        public Shell(){
+            super();
 
-    /**
-     * Constructs a {code @Shell} with with a title
-     * @param title     title of Shell created
-     */
-    public Shell(String title){
-        super(title);
+            this.children = null;
+            this.parent = null;
+        }
 
-        init(null, null);
-    }
+        /**
+         * Constructs a {@code Shell}  with
+         * a title
+         * @param title    - title of Shell created
+         */
+        public Shell(String title){
+            super(title);
 
-    /**
-     * Constructs a {code @Shell} with a starting inheritance of children Shells
-     * @param children      children Shells
-     */
-    public Shell(Shell[] children){
-        super();
+            init(null, null);
+        }
 
-        init(children, null);
-    }
+        /**
+         * Constructs a {@code Shell} with
+         * a starting inheritance of a parent Shell
+         * @param children    - children Shell(s)
+         */
+        public Shell(Shell[] children){
+            super();
 
-    /**
-     * Constructs a {code @Shell} with a starting inheritance of a parent Shell
-     * @param parent
-     */
-    public Shell(Shell parent){
-        super();
+            init(children, null);
+        }
 
-        init(null, parent);
-    }
+        /**
+         * Constructs a {@code Shell} with
+         * a starting inheritance of a parent Shell
+         * @param parent - ONE parent shell
+         */
+        public Shell(Shell parent){
+            super();
 
-    /**
-     * Constructs a {code @Shell} with
-     * a starting inheritance of children Shells
-     * and
-     * a title
-     * @param title
-     * @param children
-     */
-    public Shell(String title, Shell[] children){
-        super(title);
+            init(null, parent);
+        }
 
-        init(children, null);
-    }
+        /**
+         * Constructs a {@code Shell} with
+         * a starting inheritance of children Shells
+         * and
+         * a title
+         * @param title     - title of Shell created
+         * @param children  - children Shell(s)
+         */
+        public Shell(String title, Shell[] children){
+            super(title);
 
-    /**
-     * Constructs a {code @Shell} with
-     * 
-     * @param title
-     * @param parent
-     */
-    public Shell(String title, Shell parent){
-        super(title);
-        
-        init(null, parent);
-    }
+            init(children, null);
+        }
 
-    /**
-     * 
-     * @param children
-     * @param parent
-     */
-    public Shell(Shell[] children, Shell parent){
-        super();
+        /**
+         * Constructs a {@code Shell} with
+         * a starting inheritance of a parent Shell
+         * and
+         * a title
+         * @param title     - title of Shell created
+         * @param parent    - ONE parent Shell
+         */
+        public Shell(String title, Shell parent){
+            super(title);
 
-        init(children, parent);
-    }
+            init(null, parent);
+        }
 
-    /**
-     * 
-     * @param title
-     * @param children
-     * @param parent
-     */
-    public Shell(String title, Shell[] children, Shell parent){
-        super(title);
+        /**
+         * Constructs a {@code Shell} with
+         * a starting inheritance of children Shell(s)
+         * and
+         * a parent Shell
+         * @param children  - children Shell(s)
+         * @param parent    - ONE parent Shell
+         */
+        public Shell(Shell[] children, Shell parent){
+            super();
 
-        init(children, parent);
-    }
+            init(children, parent);
+        }
 
-    private void init(Shell[] children, Shell parent){
-        this.children = children;
-        this.parent = parent;
-    }
-}
+        /**
+         * Constructs a {@code Shell} with
+         * a starting inheritance of a parent Shell
+         * and
+         * children Shell(s)
+         * and
+         * a title
+         * @param title     - title of Shell created
+         * @param children  - children Shell(s)
+         * @param parent    - ONE parent Shell
+         */
+        public Shell(String title, Shell[] children, Shell parent){
+            super(title);
+
+            init(children, parent);
+        }
+
+        /**
+         * Initializes the {@code Shell} variables<p>
+         * Should only be used for constructors<p>
+         * 
+         * @param children  - children Shell(s)
+         * @param parent    - ONE parent Shell
+         */
+        private void init(Shell[] children, Shell parent){
+            this.children = children;
+            this.parent = parent;
+        }
+}   
