7af3009fb8c2b058d51dd0330d58ad972c8bfa36
Joseph Kennedy <josephstonekennedy.2024@gmail.com>
2024-02-01 08:40:23 -0500

    Trying JavaScript

--- JavaScript/Test.js
+++ JavaScript/Test.js
@@ -2,22 +2,31 @@
 htmlRecover = document.body
 
 function test(){
+    console.log(htmlRecover)
     document.getElementById("button").innerHTML = "goodbye :("
     console.log("waiting")
     sleep(2000).then(() => {
         console.log('done');
+        document.getElementById("button").innerHTML = "Hello! :3";
         document.write('<h2>you murderer</h2>',
-         '<button id = "button" type = "button" onclick = "back()">revive?</button>',
-        '<source> src = Test.js </source>')
-    })
-    console.log(htmlRecover)
-    
-
-
+         '<button id = "button" type = "button" onclick = "back()">revive?</button>',)
+    });
+    console.log(htmlRecover);
 }
 function back(){
-    document.body.replaceWith(htmlRecover)
-
+    document.body.replaceWith(htmlRecover);
+    document.getElementById("heading").innerHTML = "IT WORKED";
+    const newParagraph = document.createElement("p");
+    newParagraph.setAttribute('id', 'maybe?');
+    newParagraph.innerHTML = "does this work?"
+    document.body.insertBefore('paragraphs', 'maybe?');
+    try{
+        document.getElementById('maybe?').innerHTML = "kinda";
+    }catch(Error)
+    {
+        console.error(Error);
+        console.log("it lies");
+    }
 }
 
 function sleep(ms) {
